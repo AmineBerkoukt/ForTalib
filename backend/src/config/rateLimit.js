60cb14b4c4c5cf59b989dda08b63913a6f6203fd
Amine Berkoukt <amine.berkoukt@gmail.com>
2025-03-01 09:45:18 +0000

    Added rate limiting

---
+++ backend/src/config/rateLimit.js
@@ -0,0 +1,25 @@
+import rateLimit from 'express-rate-limit';
+
+export const registerLimiter = rateLimit({
+    windowMs: 30 * 60 * 1000,
+    max: 8,
+    message: { error: 'You have created many accounts !\n Try again after 30 minutes !' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+export const loginLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 5, // Max 5 requests per IP
+    message: { error: 'Too many failed logins please try again after 5 minutes !' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+export const createPostLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 3, // Max 5 requests per IP
+    message: { error: 'Too many posts !\n Try again after an hour !' },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,14 +2,12 @@ import express from 'express';
 import { validateLogin } from '../validations/login/validateLogin.js';
 import { validateSignup} from "../validations/signup/validateSignup.js";
 import {register, login, forgotPassword, resetPassword} from '../controllers/authController.js';
+import { registerLimiter, loginLimiter } from "../config/rateLimit.js";
 
 const router = express.Router();
 
-//router.post('/register', uploadPfp.single('profilePhoto'), validateRegistration, register);
-
-
-router.post('/register', validateSignup, register);
-router.post('/login', validateLogin, login);   // { email, password }
+router.post('/register', registerLimiter, validateSignup, register);
+router.post('/login', loginLimiter, validateLogin, login);   // { email, password }
 
 router.post('/forgot-password', forgotPassword);  // { email }
 router.post('/reset-password/:token', resetPassword);  // { newPassword }
