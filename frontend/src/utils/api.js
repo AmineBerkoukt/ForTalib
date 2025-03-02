ebb6be980f321d52f812e1e1bb2a02b3689fa103
Amine Berkoukt <amine.berkoukt@gmail.com>
2025-03-02 21:40:37 +0000

    Centralized api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
-const BASE_URL = "http://localhost:5000/api";
-
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const navigate = useNavigate();
 // Create an Axios instance
 const api = axios.create({
   baseURL: BASE_URL,
@@ -17,7 +18,6 @@ api.interceptors.request.use(
       return config;
     },
     (error) => {
-      // Handle errors before the request is sent
       return Promise.reject(error);
     }
 );
@@ -27,10 +27,8 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
       if (error.response && error.response.status === 401) {
-        // Handle token expiration (optional: redirect to login or refresh token)
         console.error("Unauthorized! Redirecting to login...");
-        // For example:
-        // window.location.href = "/login";
+        navigate("/login");
       }
       return Promise.reject(error);
     }
