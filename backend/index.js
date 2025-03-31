808a7ff68e68ea0e053c022613e32ad5da51ebf8
Amine Berkoukt <amine.berkoukt@gmail.com>
2025-03-31 14:11:51 +0000

    Fixed npm i issues and cleaned index.js

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,11 @@ import favoriseRoutes from './src/routes/saveRoutes.js';
 import evaluateRoutes from "./src/routes/evaluateRoutes.js";
 import banRoutes from "./src/routes/banRoutes.js";
 import passport from 'passport';
-import http from 'http';
 import cors from 'cors';
 import {app, server} from "./src/config/socket.js";
 import path from "path";
 import {fileURLToPath} from "url";
 import messageRoutes from "./src/routes/messageRoutes.js";
-//Multer config :
-import {uploadPostImages, uploadPfp} from './src/config/upload.js';
 import {authenticateToken} from './src/middlewares/authMiddleware.js';
 
 
@@ -30,9 +27,7 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
 
 
 
@@ -56,7 +51,6 @@ app.use(passport.initialize());
 app.use(authenticateToken);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-//Auth routes :
 
 // Routes
 app.use('/api/auth', oauth2Routes);
@@ -71,65 +65,7 @@ app.use('/api/messages', messageRoutes);
 app.use('/api/banned-users', banRoutes);
 
 
-let fileName = "";
-
-
-// Route to test file upload
-app.post('/api/uploadPost', uploadPostImages.single("media"), (req, res) => {
-    console.log("req.file", req.file)
-    if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded. Please provide a valid file.' });
-    }
-
-    //file.path is to store in the db !
-
-    fileName = req.file.filename;
-    const fileInfo = {
-        fileName: req.file.filename,
-        path: req.file.path,
-    };
-
-    res.status(200).json({
-        message: "File uploaded successfully!",
-        file: fileInfo,
-    });
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-/*
-app.post('/uploadPfp', uploadPfp.single('image'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-    }
-
-    res.status(200).json({
-        message: `File uploaded !`,
-        path: req.file.path,
-    });
-});
-*/
-
-
-app.get('/api/getImage', (req, res) => {
-    try {
-        if (!fileName) {
-            return res.status(404).json({ message: 'No image found. Please upload an image first.' });
-        }
-
-        const networkProtocol = req.protocol;
-
-        const hostName = req.get('host');
-
-        console.log("User id : ", req.user.id)
-        // Convert the file path to a URL-friendly format
-        const fileUrl = `${networkProtocol}://${hostName}/uploads/${req.user.id}/posts/${fileName}`;
-
-        res.status(200).json({ imageUrl: fileUrl });
-    } catch (error) {
-        console.error("Error retrieving image:", error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
-
-
 
